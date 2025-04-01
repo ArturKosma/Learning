fee8b11fc44adadeb0314342647eb90f78baf97b
ArturKosma <garond10@windowslive.com>
2025-04-01 19:56:47 +0200

    bat deploy

--- docs/animFlex.js
+++ docs/animFlex.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpjoyti9we.js
+// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpcu3k4eus.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -215,21 +215,21 @@ Module['FS_createPath']("/content", "textures", true, true);
 
   })();
 
-// end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpjoyti9we.js
-// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpqyqz30g1.js
+// end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpcu3k4eus.js
+// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpn1d10ynf.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpqyqz30g1.js
-// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpgglw_qsi.js
+  // end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpn1d10ynf.js
+// include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpcmen3ek_.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpgglw_qsi.js
+  // end include: C:\Users\LEQUEN~1\AppData\Local\Temp\tmpcmen3ek_.js
 
 
 // Sometimes an existing Module object exists with properties
