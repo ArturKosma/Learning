dbc4b49023cb2b2201583ecf4c20b0b2eb001d9a
ArturKosma <leq.artur.kosma@gmail.com>
2025-06-12 22:41:41 +0200

    improved the gltf exporting, got rid of + and - in names

--- scripts/maya/massGLTFAnim.py
+++ scripts/maya/massGLTFAnim.py
@@ -16,10 +16,18 @@ destination_folder = cmds.fileDialog2(fileMode=3, caption="Select Export Target"
 if destination_folder:
     print(destination_folder)
 
+# Set maya timeline to 30fps.    
+cmds.currentUnit(time='ntsc')
+
 # Loop through all selected files.    
 if file_paths:
     for path in file_paths:
+        
+        # Get pure filename.
         filename = os.path.splitext(os.path.basename(path))[0]
+        
+        # Sanitize filename: + → p, - → m.
+        safe_filename = filename.replace('+', 'p').replace('-', 'm')
         
         # Clear before import.  
         ClearScene()
@@ -28,7 +36,7 @@ if file_paths:
         cmds.file(path, i=True)
 
         # Create export path.
-        export_path = os.path.join(destination_folder[0], filename + ".gltf")
+        export_path = os.path.join(destination_folder[0], safe_filename + ".gltf")
         print(export_path)
         
         # Export GLTF.
