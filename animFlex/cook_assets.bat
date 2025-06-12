09ec4fec26cd761298ac3192e52c7dad348cfc56
ArturKosma <leq.artur.kosma@gmail.com>
2025-06-12 15:44:23 +0200

    working on cooker

---
+++ animFlex/cook_assets.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Anims source path.
+set "ANIMS_SOURCE=%~dp0%artassets\anims"
+
+REM Anims target path.
+set "ANIMS_EXPORT=%~dp0%export\anims"
+
+REM Go to packager exe.
+cd ../animFlex_cooking
+
+REM Order packaging per type.
+animFlex_cooking.exe anims %ANIMS_SOURCE% %ANIMS_EXPORT%
+
+pause
